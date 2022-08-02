b3c259a83bb87fd674879637a1c3a7bb17246ecd
mario lujan <mario_lujan88@hotmail.com>
2022-08-02 18:55:09 -0300

    first commit

---
+++ pt09-M1/01-IntroToCS/homework/homework.js
@@ -0,0 +1,41 @@
+'use strict'
+
+function BinarioADecimal(num) {
+
+  let miArray = Array.from (String(num),Number);
+let arrayInvert = miArray.reverse();
+let arrayResult = [];
+let resultado = 0;
+
+for (var i= 0; i< arrayInvert.length; i++){
+  arrayResult.push (arrayInvert[i] * Math.pow(2,i));
+}
+
+for (var i= 0; i< arrayResult.length; i++){
+  resultado = resultado + arrayResult[i];
+}
+return resultado;
+
+}
+
+function DecimalABinario(num) {
+  
+  let arrayBinario = [];
+
+while (num > 1){
+  arrayBinario.push(num % 2);
+  num = Math.floor(num / 2);
+}
+
+if (num === 1) arrayBinario.push(1);
+arrayBinario.reverse();
+var resultado =+ arrayBinario.join("");
+return resultado.toString();
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
