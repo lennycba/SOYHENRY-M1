7ba55daba641f69b58cda781b615281b73144abe
mario lujan <mario_lujan88@hotmail.com>
2022-08-18 00:54:34 -0300

    tarea estructura de datos II primera parte

--- pt09-M1/01-IntroToCS/homework/homework.js
+++ pt09-M1/01-IntroToCS/homework/homework.js
@@ -17,6 +17,30 @@ for (var i= 0; i< arrayResult.length; i++){
 return resultado;
 
 
+var creaFuncion = function(){
+  var arreglo = [];
+
+  for ( var i=0; i < 3; i++){
+    arreglo.push(
+      function(){
+        console.log(i);
+      }
+    )
+  }
+  return arreglo;
+}
+
+var arr = creaFuncion();
+
+arr[0]() // 3 sale un 3, qué esperaban ustedes??
+arr[1]() // 3
+arr[2]() // 3
+
+
+
+
+
+
 
 }
 
