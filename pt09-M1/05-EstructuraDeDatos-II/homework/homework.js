02da5a961a609f32ebf947257a1e19fc99bdd53a
mario lujan <mario_lujan88@hotmail.com>
2022-08-18 00:52:04 -0300

    tarea estructuraDeDatos-II primera parte

--- pt09-M1/05-EstructuraDeDatos-II/homework/homework.js
+++ pt09-M1/05-EstructuraDeDatos-II/homework/homework.js
@@ -11,9 +11,76 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function LinkedList() {
+this._length = 0;
+this.head = null;
+}
 
-function Node(value) {}
+function Node(value) {
+this.value = value;
+this.next = null;
+}
+
+//-------------------------------------------------------------------------------------
+LinkedList.prototype.add = function(data){
+  let nodo = new Node(data); //instancion un nuevo nodo con el value data
+  var current = this.head; //le asigno a current el valor de head
+
+  //si la lista está vacía
+
+  if (!current){  //si current es null quiere decir que la lista esta vacia
+    this.head = nodo; //entonces le asigno nodo al head
+    this._length ++;  //e incremento length
+    return nodo;      //finalmente retorno el nodo para cortar la ejecución
+  };
+
+  //si la lista tiene al menos algun nodo
+  //head 1 -> 5 -> 3 -> null
+  //current = head = 1 ==> current.next = 5 (no es null) ==> current = current.next
+  //current = 5 ==> current.next = null ===> mi current es el ultimo valor de la lista
+  //current.next = nodo agrega un nodo despues de mi current actual (5), o sea, al final de la lista.
+  
+
+  while (current.next){ //mientras adelante del current haya otro nodo
+    current = current.next; //avanza al siguiente nodo
+  }
+    current.next = nodo;
+    this._length ++;
+    return nodo;
+};
+//----------------------------------------------------------------------------------------
+LinkedList.prototype.remove = function(){
+  var current = this.head;
+  if (this._length === 0) return null; //si length es 0 quiere decir que la lista esta vacia
+    else if (this._length === 1){ //si length es 1 hay un solo nodo en la lista
+      var auxiliar = current.value; //guardamos el valor del nodo
+      this.head = null; //nulleamos el head, ahora la lista quedo vacía
+      this._length --; //ponemos en 0 el length
+      return auxiliar; //retornamos el valor del nodo que borramos
+    }
+    //head -> 1 -> 3 -> 5 -> null
+    //             ^ //el current.next es 5 y el siguiente es null, por ende borramos current.next
+    while(current.next.next){ //tenemos que mirar dos pasos hacia adelante
+      current = current.next; //pero avanzamos un paso a la vez
+    }
+     auxiliar = current.next.value; //guardamos el VALOR del nodo antes de borrarlo
+    current.next = null; //borramos el nodo
+    this._length --;  //reducimos el length
+    return auxiliar;  //retornamos el VALOR del nodo borrado
+  };
+//------------------------------------------------------------------------------------------
+LinkedList.prototype.search = function(value){
+  var current = this.head;
+    if (!current) return null;
+      while (current){
+          if (current.value === value) return current.value;
+            else if (typeof (value) === 'function'){
+              if (value(current.value)) return current.value;
+            }
+        current = current.next;
+      }
+  return null;
+};
 
 /*
 Implementar la clase HashTable.
