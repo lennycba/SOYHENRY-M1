118d5ade9e70aadc4d681d744ec71deb3511d735
mario lujan <mario_lujan88@hotmail.com>
2022-08-31 22:44:16 -0300

    algoritmos II resuelto

--- pt09-M1/08-Algoritmos-II/homework/homework.js
+++ pt09-M1/08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,51 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
+  var pivote = array[Math.floor(Math.random() * array.length)];
+  
+  let left = []; 
+  let right = [];
+  let igual = [];
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] < pivote){
+      left.push(array[i])
+    }else if(array[i] > pivote){
+      right.push(array [i])
+    }else{
+      igual.push(array[i])
+    }
+  }
+
+  return quickSort(left).concat(igual).concat(quickSort(right));
+};
+//-----------------------------------------------------------------
+let split = function(array){
+let mitad = Math.floor(array.length / 2);
+let izq = array.slice(0, mitad);
+let der = array.slice(mitad);
+
+return [izq, der];
+};
+
+let unir = function(izq, der){
+  let izqIndex = 0;
+  let derIndex = 0;
+  let unidos = [];
+
+    while(izqIndex < izq.length && derIndex < der.length){
+      if (izq[izqIndex] < der[derIndex]){
+        unidos.push(izq[izqIndex]);
+        izqIndex ++
+      }else{
+        unidos.push(der[derIndex]);
+        derIndex ++;
+      }
+    }
+    return unidos.concat(izq.slice(izqIndex)).concat(der.slice(derIndex));
 }
 
 function mergeSort(array) {
@@ -14,7 +58,14 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+if (array.length === 1) return array;
 
+  let division = split(array);
+
+  let izq = division[0];
+  let der = division[1];
+
+  return unir(mergeSort(izq),mergeSort(der));
 }
 
 // No modificar nada debajo de esta línea
